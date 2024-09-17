b6f78d13e6d54e2008ebf7ad86ff96d250e9df62
tranthihuong-753 <dhhuongdhlt1@gmail.com>
2024-09-17 09:38:11 +0700

    .

---
+++ netbean_17_BuoiHocDauTien/src/netbean_17_buoihocdautien/Netbean_17_BuoiHocDauTien.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package netbean_17_buoihocdautien;
+
+/**
+ *
+ * @author dhhuo
+ */
+public class Netbean_17_BuoiHocDauTien {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("ha ha ha hà há hả hã hạ");
+    }
+}
